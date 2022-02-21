19550e6a058165d967323011618af8e1a3479627
Sakshi Bhongade <90645072+bsakshi831@users.noreply.github.com>
2022-02-21 23:33:22 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,43 @@
+async function dataa(event) {
+    const showcity = document.getElementById("showcity");
+    const weather = document.getElementById("weather");
+    const temp = document.getElementById("temp");
+    const minmax = document.getElementById("min");
+    const maxmin = document.getElementById("min");
+    const wind = document.getElementById("wind");
+    document.getElementById("showdata").classList.remove("hide");
+    event.preventDefault();
+    let city = event.target["city"].value;
+    console.log(city);
+    const det = await fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&appid=5ea48567760b12859fe7fa46e3abb151"
+    );
+    const respdata = await det.json();
+    showcity.innerText = respdata["name"];
+    weather.innerText =
+        "Weather :: " + respdata["weather"][0]["description"];
+    temp.innerText =
+        "TEMP TODAY :: " + to_cel(respdata["main"]["temp"]) + " °C";
+    maxmin.innerText =
+        "   MAX TEMP :: " + to_cel(respdata["main"]["temp_max"]) + " °C";
+    minmax.innerText =
+        "MIN TEMP :: " + to_cel(respdata["main"]["temp_min"]) + " °C";
+    wind.innerText = "WIND TODAY :: " + respdata["wind"]["speed"] + " m/s";
+}
+
+function to_cel(tempp) {
+    return (tempp - 273.15).toPrecision(3);
+}
+
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.sendFile('./index.html', { root: __dirname });
+})
+
+app.listen(port, () => {})
